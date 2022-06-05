b1a7a19a5ba83840265fc7b96fb0dc35906dbb2a
ishitasequeira <ishiseq29@gmail.com>
2022-06-05 14:59:34 -0400

    Start create command
    Signed-off-by: ishitasequeira <ishiseq29@gmail.com>

--- cmd/argocd/commands/applicationset.go
+++ cmd/argocd/commands/applicationset.go
@@ -1,11 +1,20 @@
 package commands
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 
+	"github.com/argoproj/argo-cd/v2/cmd/argocd/commands/headless"
+	cmdutil "github.com/argoproj/argo-cd/v2/cmd/util"
 	argocdclient "github.com/argoproj/argo-cd/v2/pkg/apiclient"
+	"github.com/argoproj/argo-cd/v2/pkg/apiclient/applicationset"
+	"github.com/argoproj/argo-cd/v2/util/errors"
+	argoio "github.com/argoproj/argo-cd/v2/util/io"
 	"github.com/argoproj/argo-cd/v2/util/templates"
 )
 
@@ -37,7 +46,8 @@ func NewAppSetCommand(clientOpts *argocdclient.ClientOptions) *cobra.Command {
 // NewApplicationCreateCommand returns a new instance of an `argocd appset create` command
 func NewApplicationSetCreateCommand(clientOpts *argocdclient.ClientOptions) *cobra.Command {
 	var (
-		fileURL string
+		appOpts cmdutil.AppOptions
+		upsert  bool
 	)
 	var command = &cobra.Command{
 		Use:   "create ApplicationSet",
@@ -48,12 +58,36 @@ func NewApplicationSetCreateCommand(clientOpts *argocdclient.ClientOptions) *cob
 	
 `,
 		Run: func(c *cobra.Command, args []string) {
-			c.HelpFunc()(c, args)
-			os.Exit(1)
+			argocdClient := headless.NewClientOrDie(clientOpts, c)
+			fileUrl := args[0]
+			appsets, err := cmdutil.ConstructApplicationSet(fileUrl)
+			errors.CheckError(err)
+
+			for _, appset := range appsets {
+				if appset.Name == "" {
+					c.HelpFunc()(c, args)
+					os.Exit(1)
+				}
+
+				conn, appIf := argocdClient.NewApplicationSetClientOrDie()
+				defer argoio.Close(conn)
+				appSetCreateRequest := applicationset.ApplicationSetCreateRequest{
+					Applicationset: appset,
+					Upsert:         upsert,
+					Validate:       appOpts.Validate,
+				}
+				created, err := appIf.Create(context.Background(), &appSetCreateRequest)
+				errors.CheckError(err)
+				fmt.Printf("application set '%s' created\n", created.ObjectMeta.Name)
+			}
+			log.Printf("AppSet Create command %s", strings.Join(args, " "))
 		},
 	}
-	command.Flags().StringVarP(&fileURL, "file", "f", "", "Filename or URL to Kubernetes manifests for the ApplicationSet")
-
+	// command.Flags().StringVarP(&fileURL, "file", "f", "", "Filename or URL to Kubernetes manifests for the ApplicationSet")
+	// err := command.Flags().SetAnnotation("file", cobra.BashCompFilenameExt, []string{"json", "yaml", "yml"})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	return command
 }
 
@@ -67,6 +101,7 @@ func NewApplicationSetListCommand(clientOpts *argocdclient.ClientOptions) *cobra
   argocd appset list`,
 		Run: func(c *cobra.Command, args []string) {
 			c.HelpFunc()(c, args)
+			log.Printf("AppSet List command %s", strings.Join(args, " "))
 			os.Exit(1)
 		},
 	}
@@ -84,6 +119,8 @@ func NewApplicationSetUpdateCommand(clientOpts *argocdclient.ClientOptions) *cob
 		Short: "Updates the given applicationSet",
 		Run: func(c *cobra.Command, args []string) {
 			c.HelpFunc()(c, args)
+			log.Printf("AppSet Update command %s", strings.Join(args, " "))
+			log.Printf("ApplicationSet Name %s", appsetName)
 			os.Exit(1)
 		},
 	}
@@ -101,6 +138,8 @@ func NewApplicationSetDeleteCommand(clientOpts *argocdclient.ClientOptions) *cob
 		Short: "Delete an applicationSet",
 		Run: func(c *cobra.Command, args []string) {
 			c.HelpFunc()(c, args)
+			log.Printf("AppSet Delete command %s", strings.Join(args, " "))
+			log.Printf("ApplicationSet Name %s", appsetName)
 			os.Exit(1)
 		},
 	}
