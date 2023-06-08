0b8cb83090053d1ac83b77e18f070af5781f75e2
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2023-06-08 11:04:34 -0400

    re-add filter
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- applicationset/controllers/applicationset_controller.go
+++ applicationset/controllers/applicationset_controller.go
@@ -675,7 +675,7 @@ func (r *ApplicationSetReconciler) createInCluster(ctx context.Context, applicat
 func (r *ApplicationSetReconciler) getCurrentApplications(_ context.Context, applicationSet argov1alpha1.ApplicationSet) ([]argov1alpha1.Application, error) {
 	// TODO: Should this use the context param?
 	var current argov1alpha1.ApplicationList
-	err := r.Client.List(context.Background(), &current)
+	err := r.Client.List(context.Background(), &current, client.MatchingFields{".metadata.controller": applicationSet.Name})
 
 	if err != nil {
 		return nil, err
