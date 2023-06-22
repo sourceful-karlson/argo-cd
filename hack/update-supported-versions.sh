e7eb247fc72b9120bcfff6212679304e50e35d20
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2023-06-22 18:08:37 -0400

    docs: list tested Kubernetes versions (#12276)
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    
    remove crud
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    
    remove crud
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    
    so-called readability
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    
    workflow
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

---
+++ hack/update-supported-versions.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "| Argo CD version | Kubernetes versions |" > docs/operator-manual/tested-kubernetes-versions.md
+echo "|-----------------|---------------------|" >> docs/operator-manual/tested-kubernetes-versions.md
+
+argocd_minor_version=$(git rev-parse --abbrev-ref HEAD | sed 's/release-//')
+argocd_major_version_num=$(echo "$argocd_minor_version" | sed -E 's/\.[0-9]+//')
+argocd_minor_version_num=$(echo "$argocd_minor_version" | sed -E 's/[0-9]+\.//')
+
+for n in 0 1 2; do
+  minor_version_num=$((argocd_minor_version_num - n))
+  minor_version="${argocd_major_version_num}.${minor_version_num}"
+  git checkout "release-$minor_version" > /dev/null || exit 1
+  yq '.jobs["test-e2e"].strategy.matrix["k3s-version"][]' .github/workflows/ci-build.yaml | \
+    jq --arg minor_version "$minor_version" --raw-input --slurp --raw-output \
+    'split("\n")[:-1] | map(sub("\\.[0-9]+$"; "")) | join(", ") | "| \($minor_version) | \(.) |"' \
+    >> docs/operator-manual/tested-kubernetes-versions.md
+done
+
+git checkout "release-$argocd_minor_version"
+
+echo >> docs/operator-manual/tested-kubernetes-versions.md
